68e3689198889cea1507fc698d786a6093bd9403
thamaraikannan25 <thamaraikannan.arumugam@tensure.io>
2026-05-29 15:59:09 +0530

    feat: add simple React login functionality (closes #22)

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <LoginForm />
+    </div>
+  );
+}
