8011e359a3afc4883ef722698ae20f5270318dc2
thamaraikannan25 <thamaraikannan.arumugam@tensure.io>
2026-06-06 02:04:42 +0530

    Add think.js file (fixes #29)

--- think.js
+++ think.js
@@ -1,9 +1,6 @@
 // think.js
 
 function think(thought) {
-  if (thought == null) {
-    throw new Error('thought must not be null or undefined');
-  }
   console.log(`Thinking: ${thought}`);
   return thought;
 }
